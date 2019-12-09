be6256d5ae1d414d989b0532fe24dc881832b70b
Ahat <ahat.gokce@gmail.com>
2019-12-09 19:15:19 +0100

    adding

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -2,9 +2,12 @@ package tests;
 
 public class task {
   
-  public static void main(String[] args){
+  public static void main(String[] args) {
     System.out.println("Hello World");
     //this line is added from GitHub,see you in Intellij
+  }
 
+  public void newMethod(){
+    System.out.println("hi");
   }
 }
